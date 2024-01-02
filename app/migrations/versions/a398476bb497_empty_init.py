e9aee4e331f9eed41da6780e9d07c8f5fe4ca3b1
root <root@DESKTOP-UEO62DE.localdomain>
2024-01-02 09:52:52 +0300

    Complete Alembic migration

---
+++ app/migrations/versions/a398476bb497_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: a398476bb497
+Revises: 
+Create Date: 2024-01-02 09:05:16.091668
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'a398476bb497'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
